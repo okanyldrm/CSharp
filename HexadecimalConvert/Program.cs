7ca3ef43a535dd7a36ddac379494aeeb75d979a0
okanyldrm <okanyldrm68@gmail.com>
2021-04-02 18:44:19 +0300

    linq hexa

---
+++ HexadecimalConvert/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace HexadecimalConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter Hexa Data");
+           var hexaString=Console.ReadLine();
+
+            var data = Converting(hexaString);
+            var result=HexaConvert(data);
+            foreach (var item in result)
+            {
+                Console.Write(item);
+            }
+        }
+
+
+
+        public static List<int> Converting(string hexadecimalData)
+        {
+           
+            List<int> forthData = new List<int>();
+
+            for (int i = 0; i < hexadecimalData.Length/4; i++)
+            {
+                var item = Int32.Parse(hexadecimalData.Substring(i * 4, 4));
+                forthData.Add(item);
+            }
+
+            return forthData;
+        }
+
+        public static List<string> HexaConvert(List<int> data)
+        {
+            List<string> result = new List<string>();
+
+            foreach (var item in data)
+            {
+                switch (item)
+                {
+                    case 0000:
+                        result.Add("0");
+                        break;
+                    case 0001:
+                        result.Add("1");
+                        break;
+                    case 0010:
+                        result.Add("2");
+                        break;
+                    case 0011:
+                        result.Add("3");
+                        break;
+                    case 0100:
+                        result.Add("4");
+                        break;
+                    case 0101:
+                        result.Add("5");
+                        break;
+                    case 0110:
+                        result.Add("6");
+                        break;
+                    case 0111:
+                        result.Add("7");
+                        break;
+                    case 1000:
+                        result.Add("8");
+                        break;
+                    case 1001:
+                        result.Add("9");
+                        break;
+                    case 1010:
+                        result.Add("A");
+                        break;
+                    case 1011:
+                        result.Add("B");
+                        break;
+                    case 1100:
+                        result.Add("C");
+                        break;
+                    case 1101:
+                        result.Add("D");
+                        break;
+                    case 1110:
+                        result.Add("E");
+                        break;
+                    case 1111:
+                        result.Add("F");
+                        break;
+                }
+            }
+
+            return result;
+
+        }
+
+
+
+    }
+}
---
+++ LinqLinq/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            List<Product> Products = new List<Product>();
+            Products.Add(new Product() {Id = 1,Name = "product1",CategoryId = 1,Price = 100});
+            Products.Add(new Product() { Id = 2, Name = "product2", CategoryId = 1, Price = 200 });
+            Products.Add(new Product() { Id = 3, Name = "product3", CategoryId = 2, Price = 300 });
+            Products.Add(new Product() { Id = 4, Name = "product4", CategoryId = 2, Price = 400 });
+            Products.Add(new Product() { Id = 5, Name = "product5", CategoryId = 2, Price = 500 });
+
+            var model = Products.Where(p => p.CategoryId == 1)
+                .Select(pr => pr.Name);
+         
+               
+
+            foreach (var mod in model)
+            {
+                Console.WriteLine(mod);
+            }
+          
+            //eager nad layz
+          
+
+           
+
+
+
+
+        }
+    }
+}
